48e8bc9344d722e00c4de505eee97334fba0c2c1
MrAtEase <59176130+MrAtEase@users.noreply.github.com>
2019-12-30 02:02:25 +0100

    Add files via upload
    This is my first freecodecamp landing page Project

---
+++ Freecodecamp landingpageproject/js/script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+    
+  $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+      
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+      
+ 
+        
+ 
